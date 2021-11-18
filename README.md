31d76eccb71036b4bbe580fc098696373ec34e3e
Julianne McCoun <jmccoun7@gmail.com>
2021-11-19 08:46:11 +1300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # cruella
+This is the week 6 project for Julianne, Troy, Josh and Tane. 
+
+We're gonna build something coolios. 
---
+++ client/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import { Route } from 'react-router-dom'
+
+// import other components:
+
+function App () {
+  return (
+    <div className='app'>
+      <h1>Hello World!</h1>
+    </div>
+  )
+}
+
+export default App
