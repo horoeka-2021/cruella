dfd7f6fcf76ef2d066dc2a0c02030463fa050e67
troy-may <troy.may@fortuneunmasked.com>
2021-11-19 10:56:31 +1300

    weird css

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -3,15 +3,22 @@ import { Link } from 'react-router-dom'
 
 function Header () {
   return (
-    <div className='nav'>
-      <nav>
-        <ul>
-          <Link to='/'>Home</Link>
-          <Link to='/about'>About</Link>
-          <Link to='/addSkill'>Add your world&apos;s best skill</Link>
-        </ul>
-      </nav>
-    </div>
+    <>
+      <h1 className='app'>
+        <span className='fa fa-thermometer' />
+        {' '} Creulla&apos;s {' '}
+        <span className='fa fa-flask' />
+      </h1>
+      <div className='nav'>
+        <nav>
+          <ul>
+            <Link to='/'>Home</Link>
+            <Link to='/about'>About</Link>
+            <Link to='/addSkill'>Add Skill</Link>
+          </ul>
+        </nav>
+      </div>
+    </>
   )
 }
 
--- client/components/PersonListItem.jsx
+++ client/components/PersonListItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function PersonListItem (props) {
   return (
-    <div className="person-list-item">
+    <div className="product">
       <h1>I am a person list item!</h1>
     </div>
   )
