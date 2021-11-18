8a273bcc247d9ead3f03b31b541d56bbfa94bb9d
tane-git <tanelives@gmail.com>
2021-11-19 12:59:22 +1300

    weird bug with get story

--- client/components/PersonListItem.jsx
+++ client/components/PersonListItem.jsx
@@ -1,26 +1,26 @@
 import React, { useState, useEffect } from 'react'
 
-import { getStory } from '../api/people'
+// import { getStory } from '../api/people'
 
 function PersonListItem (props) {
   const { name, skill, story } = props.person
 
-  const [generatedOutput, setGeneratedOutput] = useState()
+  // const [generatedOutput, setGeneratedOutput] = useState()
 
-  useEffect(async () => {
-    try {
-      setGeneratedOutput(await getStory(story))
-    } catch (error) {
-      console.error(error)
-    }
-  }, [])
+  // useEffect(async () => {
+  //   try {
+  //     setGeneratedOutput(await getStory(story))
+  //   } catch (error) {
+  //     console.error(error)
+  //   }
+  // }, [])
 
-  console.log(generatedOutput)
   return (
     <div className="person-list-item">
       <p className='name'>{name}</p>
       <p className='skill'>Skill: {skill}</p>
-      <p className='story'>Story: {generatedOutput?.output}</p>
+      {/* <p className='story'>Story: {generatedOutput?.output}</p> */}
+      <p className='story'>Story: {story}</p>
     </div>
   )
 }
--- client/utils/formatPerson.js
+++ client/utils/formatPerson.js
@@ -1,15 +1,19 @@
-// import { randomizerpi } from '../api/random?'
+import { getStory } from '../api/people'
 
 // takes in a person object and returns a person object where the person.story has been changed to include the name and skill as well as the original story.
 function formatPerson (person) {
   const formattedPerson = { ...person }
   const storyForRandomizer = `${person.name} is the best in the world at ${person.skill} because ${person.story}`
 
-  // call api to get random story
-  // const randomStory = randomizerApi.getRandomStory(storyForRandomizer)
-  const randomStory = storyForRandomizer
+  // call the getStory function to get a story for the person
+  getStory(storyForRandomizer)
+    .then(story => {
+      formattedPerson.story = story.output
+      return null
+    })
+    .catch(error => console.error(error.message))
 
-  formattedPerson.story = randomStory
+  console.log(formattedPerson)
 
   return formattedPerson
 }
