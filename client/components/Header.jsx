e76de09bbf4b320d12b9e7151a3f37a384bca0e5
tane-git <tanelives@gmail.com>
2021-11-19 09:24:32 +1300

    add header (correct)

---
+++ client/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Header () {
+  return (
+    <div className='header'>
+      <nav>
+        <ul>
+          <Link to='/'>Home</Link>
+          <Link to='/about'>About</Link>
+          <Link to='/addSkill'>Add your world&apos;s best skill</Link>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Header
