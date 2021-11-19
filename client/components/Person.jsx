c0550b01917f9490abc87f8b3c6ffa29ed390d3f
joshuawc <joshkosh99@gmail.com>
2021-11-19 14:22:57 +1300

    Add profile pics

--- client/components/Person.jsx
+++ client/components/Person.jsx
@@ -8,9 +8,18 @@ function Person (props) {
   console.log('Person id: ', id)
   const person = props.people.find(person => person.id === id)
   console.log('Person person: ', person)
+
+
+  const pictureUrlJPG = `/images/${person.name.toLowerCase()}.jpg`
+  const pictureUrlJPEG = `/images/${person.name.toLowerCase()}.jpeg`
+  const pictureUrlPNG = `/images/${person.name.toLowerCase()}.png`
+
   return (
     <div>
       <h1>I am a person</h1>
+      <img src={pictureUrlJPG} alt="" />
+      <img src={pictureUrlJPEG} alt="" />
+      <img src={pictureUrlPNG} alt="" />
       <p>
         I am {person.name} My skill is {person.skill} and my story is {person.story}
       </p>
